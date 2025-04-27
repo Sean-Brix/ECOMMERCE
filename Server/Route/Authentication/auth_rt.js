e254d0c6224b900c23429f38f67a9b3696439347
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-04-27 23:36:17 +0800

    v1.12.0 - (Router/Model) Server structure changes and .env pairs addition

---
+++ Server/Route/Authentication/auth_rt.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+// Route: ('/auth')
+const router = express.Router();
+
+// import authentication from './Authentication/auth_rt.js'
+// router.use('/other', authentication)
+
+export default router;
